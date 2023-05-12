e82340191902466fd2f491d91e2de3b22cd01312
tomassantosz <tomassantos016@gmail.com>
2023-05-12 19:07:20 -0300

    feat: add nivel in dashboard

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { HighlightCars } from '../../components/HighlightCard';
 import Nivel from '../../util/Nivel';
 import { ImagemPerfil } from '../../assets/alfabeto';
+import { api } from '../../services/api';
 import { 
   Container,
   Header,
@@ -17,10 +18,43 @@ import {
 } from './styles';
 import {useAuth} from '../../contexts/auth';
 
+interface Item {
+  _id: string;
+  nome: string;
+  esporte: {
+    nome: string;
+    Regras: string;
+  }
+  atletas: {
+    forEach:(atleta: object) => void,
+    length: any,
+  };
+  DataInicio:string;
+  DataTermino: string;
+  NumPart: number;
+
+}
+
 export function Dashboard(){
   const navigation = useNavigation();
   const {signOut, user} = useAuth();
-  console.log(user)
+  const [nivel_atual, setNivelAtual] = useState(0);
+  useEffect(()=>{    
+    const competicoesUsuario: Item[] = [];
+
+    async function fetchCompeticoes() {
+      const response = await api.get('/Competicoes');
+      response.data.forEach((competicoes:Item) => {        
+        competicoes.atletas.forEach((atleta: any) => {
+          if(atleta._id === user?._id){
+            competicoesUsuario.push(competicoes)
+          }
+        });        
+      });
+      setNivelAtual(Nivel(competicoesUsuario.length));
+    }
+    fetchCompeticoes();
+  },[nivel_atual]);
   
   function handleSignOut(){
     signOut();
@@ -42,7 +76,7 @@ export function Dashboard(){
               <Photo source={ImagemPerfil(user?.nome.substring(0,1).toUpperCase())}/>  
                 <User>
                   <UserName>{ user?.nome }</UserName>
-                  <NivelName>Nível {Nivel()}</NivelName>
+                  <NivelName>Nível {nivel_atual}</NivelName>
                 </User>
               </UserInfo>
               <Icon 
