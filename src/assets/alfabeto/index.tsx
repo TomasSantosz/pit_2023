da28f242d243459304e777dff7c2cc31be728f45
tomassantosz <tomassantos016@gmail.com>
2023-05-26 00:57:03 -0300

    fix: Image perfil refactore

--- src/assets/alfabeto/index.tsx
+++ src/assets/alfabeto/index.tsx
@@ -1,141 +1,111 @@
-
-import { Photo } from './styles';
-
-
 export function ImagemPerfil(letter:string){
 
 switch (letter) {
     case 'A':
         const A = require('./A.png');
         return (A);
-        console.log('Path for A:', A);
         break;
     case 'B':
         const B = require('./B.png');
         return (B);
-        console.log('Path for B:', B);
         break;
     case 'C':
         const C = require('./C.png');
         return (C);
-        console.log('Path for C:', C);
         break;
     case 'D':
         const D = require('./D.png');
         return (D);
-        console.log('Path for D:', D);
         break;
     case 'E':
         const E = require('./E.png');
         return (E);
-        console.log('Path for E:', E);
         break;
     case 'F':
         const F = require('./F.png');
         return (F);
-        console.log('Path for F:', F);
         break;
     case 'G':
         const G = require('./G.png');
         return (G);
-        console.log('Path for G:', G);
         break;
     case 'H':
         const H = require('./H.png');
         return (H);
-        console.log('Path for H:', H);
         break;
     case 'I':
         const I = require('./I.png');
         return (I);
-        console.log('Path for I:', I);
         break;
     case 'J':
         const J = require('./J.png');
         return (J);
-        console.log('Path for J:', J);
         break;
     case 'K':
         const K = require('./K.png');
         return (K);
-        console.log('Path for K:', K);
         break;
     case 'L':
         const L = require('./L.png');
         return (L);
-        console.log('Path for L:', L);
         break;
     case 'M':
         const M = require('./M.png');
         return (M);
-        console.log('Path for M:', M);
         break;
     case 'N':
         const N = require('./N.png');
         return (N);
-        console.log('Path for N:', N);
         break;
     case 'O':
         const O = require('./O.png');
         return (O);
-        console.log('Path for O:', O);
         break;
     case 'P':
         const P = require('./P.png');
         return (P);
-        console.log('Path for P:', P);
         break;
     case 'Q':
         const Q = require('./Q.png');
         return (Q);
-        console.log('Path for Q:', Q);
         break;
     case 'R':
         const R = require('./R.png');
         return (R);
-        console.log('Path for R:', R);
         break;
     case 'S':
         const S = require('./S.png');
         return (S);
-        console.log('Path for S:', S);
         break;
     case 'T':
         const T = require('./T.png');
         return (T);
-        console.log('Path for T:', T);
         break;
     case 'U':
         const U = require('./U.png');
         return (U);
-        console.log('Path for U:', U);
         break;
     case 'V':
         const V = require('./V.png');
         return (V);
-        console.log('Path for V:', V);
         break;
     case 'W':
         const W = require('./W.png');
         return (W);
-        console.log('Path for W:', W);
         break;
     case 'X':
         const X = require('./X.png');
         return (X);
-        console.log('Path for X:', X);
         break;
     case 'Y':
         const Y = require('./Y.png');
         return (Y);
-        console.log('Path for Y:', Y);
         break;
     case 'Z':
         const Z = require('./Z.png');
         return (Z);
-        console.log('Path for Z:', Z);
         break;
     default:
-      console.log('Invalid option');
-  }
+        console.log('Invalid option');
+    }
 }
